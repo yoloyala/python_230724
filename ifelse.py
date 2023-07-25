21fe65ce448f478d3275962ec22a9126dfbe99f7
appolo <appolo2@naver.com>
2023-07-25 11:16:38 +0900

    3장 완료

--- ifelse.py
+++ ifelse.py
@@ -42,3 +42,15 @@ print(list(range(1,32)))
 
 for i in range(10):
     print(i)
+
+print("---리스트 컴프리헨션---")
+lst = list(range(1,11))
+print([i**2 for i in lst if i > 5])
+tp = ("apple", "banana", "orange")
+print([len(i) for i in tp])
+d = {100:"apple", 200:"kiwi"}
+print([v.upper() for v in d.values()])
+
+for i in map(lambda x:x+10, lst):
+    print(i)
+
